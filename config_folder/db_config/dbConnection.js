efab7bef2390a10f90b63d65a9dfa907cd13a820
awasevalentine <awasevalentine@gmail.com>
2020-08-03 02:34:49 +0100

    added db connection strings

--- config_folder/db_config/dbConnection.js
+++ config_folder/db_config/dbConnection.js
@@ -6,7 +6,7 @@ module.exports =function() {
     
 return new Promise((resolve, reject)=>{
     try{
-        db = mongoose.connect(process.env.MONGODB_URL, (err)=>{
+        db = mongoose.connect(process.env.MONGODB_URL || dbUri , (err)=>{
             if(err){
                 throw new Error(`failed to connect to db`);
             }
